b452d080d80be5d4151ff9741f5c2a18863009fe
shreya <sharmashreya0003@gmail.com>
2026-06-01 13:43:46 +0530

    fix changes for render

--- dreamproject/settings.py
+++ dreamproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-change-me')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on git pushin production!
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS =["journaling-app-vfzf.onrender.com"]
