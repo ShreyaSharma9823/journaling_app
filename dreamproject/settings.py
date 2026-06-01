36ebed63b66d039fd04657564f1efbe5915321e0
shreya <sharmashreya0003@gmail.com>
2026-06-01 14:14:23 +0530

    render deployed

--- dreamproject/settings.py
+++ dreamproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS =["journaling-app-vfzf.onrender.com"]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
