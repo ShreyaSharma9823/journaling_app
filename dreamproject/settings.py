f5c8139e3b086b7a5beb5cf6fcecc95d80b26e01
shreya <sharmashreya0003@gmail.com>
2026-06-01 13:06:45 +0530

    render deploy

--- dreamproject/settings.py
+++ dreamproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,11 +82,9 @@ WSGI_APPLICATION = 'dreamproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': dj_database_url.parse(os.environ['DATABASE_URL'])    
     }
-}
+
 
 
 # Password validation
